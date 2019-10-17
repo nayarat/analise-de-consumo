ced3ff9a2e896600d14d03004c2a45b0dade447b
LucasFDutra <lucasfelipedutra@gamil.com>
2019-10-16 23:14:56 -0300

    corrigindo os ;

---
+++ a.py
@@ -0,0 +1,6 @@
+base_url = 'https://raw.githubusercontent.com/LucasFDutra/analise-de-consumo/master/Trafos/consumo/'
+
+consumoTrafo1 = np.genfromtxt(base_url + 'Trafo1.csv', delimiter=';')
+consumoTrafo2 = np.genfromtxt(base_url + 'Trafo2.csv', delimiter=';')
+consumoTrafo3 = np.genfromtxt(base_url + 'Trafo3.csv', delimiter=';')
+consumoTrafo4 = np.genfromtxt(base_url + 'Trafo4.csv', delimiter=';')
